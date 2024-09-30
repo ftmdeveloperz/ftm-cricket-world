cf41a21c5c666e12a451e326a199a903f56b933a
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2024-09-30 17:23:09 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+API_KEY = 'd670fa9a-8d0b-4afd-93a6-d99d2047983f'  # Your CricAPI key
+
+@app.route('/')
+def home():
+    scores = get_live_scores()  # Function to fetch live scores
+    return render_template('index.html', scores=scores)
+
+def get_live_scores():
+    url = f'https://cricapi.com/api/cricket/?apikey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    return data.get('data', [])
+
+if __name__ == '__main__':
+    app.run(debug=True)
